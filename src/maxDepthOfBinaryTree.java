a034915e9fc9c60d2945aeca37c603799dc25aaf
Aivan Tuquero <54162088+aivantuquero@users.noreply.github.com>
2020-11-23 16:32:44 +0800

    Create maxDepthOfBinaryTree.java

---
+++ src/maxDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+//recursive solution~
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        return Math.max(maxDepth(root.right),maxDepth(root.left))+1;
+    }
+}
